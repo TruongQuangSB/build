d1afa513fb8fadfd1604087544c46190d0766a6f
Quang Truong <truong.quang@scheidt-bachmann-st.de>
2024-09-11 13:59:17 +0200

    update

--- docker/update-table-reference/updatereference/constant.py
+++ docker/update-table-reference/updatereference/constant.py
@@ -6,13 +6,13 @@ class Constanst:
     GITHUB_USER = os.environ.get("GITHUB_USER")
     GITHUB_API_URL = "https://api.github.com/repos"
 
-    SET_REPO = "truongquangsb/set"
+    SET_REPO = "eclipse-set/set"
     PPT_Test_ALL_JOB_API_URL = "https://vl-jenkins-ctl.ki.lan/job/PlanPro/job/PPTTest"
 
     TABLE_REFERENCE_ARTIFACT_NAME_PATTERN = "table-csv-{}"
-    # BUILD_SET_WORK_FLOW_ID = 63604658
-    BUILD_SET_WORK_FLOW_ID = 64995101
-    SET_REMOTE_URL = "https://github.com/truongquangsb/set.git"
+    BUILD_SET_WORK_FLOW_ID = 63604658
+    # BUILD_SET_WORK_FLOW_ID = 64995101
+    SET_REMOTE_URL = "https://github.com/eclipse-set/set.git"
     SET_LOCA_REPO_PATH = "./set"
     SET_TABLE_REFERENCE_PATH = (
         "java/bundles/org.eclipse.set.swtbot/test_res/table_reference"
