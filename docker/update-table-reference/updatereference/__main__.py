4cf83ec968aa56cd299f7be7ba7a0a0259a87322
Quang Truong <truong.quang@scheidt-bachmann-st.de>
2024-09-11 17:09:24 +0200

    update

--- docker/update-table-reference/updatereference/__main__.py
+++ docker/update-table-reference/updatereference/__main__.py
@@ -22,20 +22,21 @@ def main():
     )
     if not new_reference_zip:
         raise SystemError("Can't download the new reference artifact")
-    apply_changes(branch_name, new_reference_zip)
-    shutil.rmtree(CONSTANT.SET_LOCA_REPO_PATH, onexc=on_remove_error_hanlde)
+    update_table_reference(new_reference_zip)
+    # apply_changes(branch_name, new_reference_zip)
+    # shutil.rmtree(CONSTANT.SET_LOCA_REPO_PATH, onexc=on_remove_error_hanlde)
 
 
-def apply_changes(branch_name: str, download_url: str):
-    set_repo = checkout_branch(
-        CONSTANT.SET_REMOTE_URL, CONSTANT.SET_LOCA_REPO_PATH, branch_name
-    )
-    update_table_reference(download_url)
-    if set_repo.is_dirty(untracked_files=True):
-        set_repo.git.add(A=True)
-        set_repo.index.commit(f"{branch_name} update table reference")
-        set_repo.remote().push()
-        print(f"{branch_name} update table reference")
+# def apply_changes(branch_name: str, download_url: str):
+#     set_repo = checkout_branch(
+#         CONSTANT.SET_REMOTE_URL, CONSTANT.SET_LOCA_REPO_PATH, branch_name
+#     )
+#     update_table_reference(download_url)
+#     if set_repo.is_dirty(untracked_files=True):
+#         set_repo.git.add(A=True)
+#         set_repo.index.commit(f"{branch_name} update table reference")
+#         set_repo.remote().push()
+#         print(f"{branch_name} update table reference")
 
 
 def update_table_reference(new_reference_zip):
@@ -63,29 +64,28 @@ def update_table_reference(new_reference_zip):
         raise SystemError()
 
 
-def checkout_branch(remote: str, local_repo_path: str, branch_name: str):
-    result_repo = None
-    print(f"Repo path: {local_repo_path}")
-    # When the repository is already create/clone, then check if this repo have correct remote
-    if os.path.exists(local_repo_path):
-        print("Exist local repo")
-        result_repo = Repo(local_repo_path)
-        print("Current repo remote: " + result_repo.remote().url)
-        if result_repo.remote().url != remote:
-            print("Diffrence remote repo")
-            shutil.rmtree(local_repo_path, onexc=on_remove_error_hanlde)
-            result_repo = None
+# def checkout_branch(remote: str, local_repo_path: str, branch_name: str):
+#     result_repo = None
+#     print(f"Repo path: {local_repo_path}")
+#     # When the repository is already create/clone, then check if this repo have correct remote
+#     if os.path.exists(local_repo_path):
+#         print("Exist local repo")
+#         result_repo = Repo(local_repo_path)
+#         print("Current repo remote: " + result_repo.remote().url)
+#         if result_repo.remote().url != remote:
+#             print("Diffrence remote repo")
+#             shutil.rmtree(local_repo_path, onexc=on_remove_error_hanlde)
+#             result_repo = None
 
-    if result_repo == None:
-        print(f"Clone {remote}")
-        result_repo = Repo.clone_from(remote, local_repo_path)
-    if result_repo == None:
-        raise SystemError
-    result_repo.git.execute("pull")
-    result_repo.git.execute(f"checkout {branch_name}")
-    # result_repo.git.checkout(branch_name)
-    print(result_repo.git.status())
-    return result_repo
+#     if result_repo == None:
+#         print(f"Clone {remote}")
+#         result_repo = Repo.clone_from(remote, local_repo_path)
+#     if result_repo == None:
+#         raise SystemError
+#     result_repo.remotes.origin.pull()
+#     result_repo.git.checkout(branch_name)
+#     print(result_repo.git.status())
+#     return result_repo
 
 
 def on_remove_error_hanlde(func, path, exc_info):
